5724767c78d23f5288744e3039adda547c2be29a
a <yurtaevandreyR@yandex.ru>
2024-04-22 09:00:22 +0300

    0. Что такое тестирование [#159431]

---
+++ src/main/java/ru/job4j/convert/Converter.java
@@ -0,0 +1,32 @@
+package ru.job4j.convert;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        return value / 100;
+    }
+
+    public static float rubleToDollar(float value) {
+        return value / 90;
+    }
+
+    public static boolean rubleToEuroTest(float input, float expected) {
+        return expected == rubleToEuro(input);
+    }
+
+    public static boolean rubleToDollarTest(float input, float expected) {
+        return expected == rubleToDollar(input);
+    }
+
+    public static void main(String[] args) {
+        boolean outputEuro = rubleToEuroTest(900, 9);
+        boolean outputDollar = rubleToDollarTest(900, 10);
+
+        System.out.println("900 rubles are 9 euros. Test result: " + outputEuro);
+        System.out.println("900 rubles are 10 dollars. Test result: " + outputDollar);
+
+    }
+
+}
+
+
