be81ed48d37152a4b0490652350be8d4ae1d7562
yurtaevandreyR <yurtaevandreyR@yandex.ru>
2024-10-07 23:28:31 +0300

    4.1. Операторы ветвлений [#3611]

---
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void checkNumber(int number) {
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
+        if (number < 0) {
+            System.out.println("This is negative number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        AlertDivByZero.checkNumber(5);
+        AlertDivByZero.checkNumber(0);
+        AlertDivByZero.checkNumber(-5);
+    }
+}
