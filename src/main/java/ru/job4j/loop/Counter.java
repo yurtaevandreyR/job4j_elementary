c2e0726443e2dc984782d1d4147b8e3b370b4c24
yurtaevandreyR <yurtaevandreyR@yandex.ru>
2024-10-27 22:21:42 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,10 +9,13 @@ public class Counter {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(1, 3));
-        System.out.println(sum(1, 4));
-        System.out.println(sum(3, 4));
-        System.out.println(sum(5, 3));
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -2,7 +2,7 @@ package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import static ru.job4j.loop.Counter.sum;
+import static ru.job4j.loop.Counter.*;
 
 class CounterTest {
 
@@ -34,10 +34,55 @@ class CounterTest {
     }
 
     @Test
-    void whenSum5To3Then6() {
+    void whenSum5To3Then0() {
         int start = 5;
         int finish = 3;
         int result = sum(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEven1To10Then30() {
+        int start = 1;
+        int finish = 10;
+        int result = sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenMinus3To3Then0() {
+        int start = -3;
+        int finish = 3;
+        int result = sumByEven(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenMinus10ToMinus1ThenMinus30() {
+        int start = -10;
+        int finish = -1;
+        int result = sumByEven(start, finish);
+        int expected = -30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEven10To20Then90() {
+        int start = 10;
+        int finish = 20;
+        int result = sumByEven(start, finish);
+        int expected = 90;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEven7To3Then0() {
+        int start = 7;
+        int finish = 3;
+        int result = sumByEven(start, finish);
         int expected = 0;
         assertThat(result).isEqualTo(expected);
     }
